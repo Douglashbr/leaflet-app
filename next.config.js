bd2f804eee129ccabc6f132b7520caf5fe5acaf0
Douglas Dantas <douglas.mdantas@hotmail.com>
2021-08-18 23:08:25 -0300

    Adicionar o Leaflet e criando os testes para o componente Map

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')([
+  'react-leaflet',
+  '@react-leaflet/core'
+])
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
+module.exports = module.exports = withPlugins([
+  [
+    withTM,
+    {
+      future: {
+        webpack5: true
+      }
+    }
+  ],
+  [
+    withPWA,
+    {
+      pwa: {
+        dest: 'public',
+        disable: !isProd
+      }
+    }
+  ]
+])
